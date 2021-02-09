053838fe1698ab11f34aa4f0664fbdbc8540dd38
Michael <57787676+baentsch@users.noreply.github.com>
2021-02-09 14:41:31 +0100

    Changing python platform test
    Fixes #901

--- tests/test_portability.py
+++ tests/test_portability.py
@@ -11,7 +11,7 @@ MIN_CPUS["x86_64"] = "Westmere"
 
 @helpers.filtered_test
 @pytest.mark.parametrize('kem_name', helpers.available_kems_by_name())
-@pytest.mark.skipif(not "Ubuntu" in platform.platform(), reason="Only supported on Ubuntu")
+@pytest.mark.skipif(not "Ubuntu" in platform.uname().version, reason="Only supported on Ubuntu")
 def test_kem(kem_name):
     if not(helpers.is_build_portable()):
         pytest.skip("Portability not enabled")
@@ -19,7 +19,7 @@ def test_kem(kem_name):
     if not(helpers.is_kem_enabled_by_name(kem_name)):
         pytest.skip('Not enabled')
 
-    helpers.run_subprocess(["qemu-"+platform.machine(), "-cpu", MIN_CPUS[platform.machine()],
+    helpers.run_subprocess(["qemu-"+platform.machine()+"-static", "-cpu", MIN_CPUS[platform.machine()],
                             helpers.path_to_executable('test_kem'), kem_name])
 
 @helpers.filtered_test
@@ -35,7 +35,7 @@ def test_sig(sig_name):
     if (sig_name.startswith("picnic")):
         pytest.skip("Picnic portability known to not be given.")
 
-    helpers.run_subprocess(["qemu-"+platform.machine(), "-cpu", MIN_CPUS[platform.machine()],
+    helpers.run_subprocess(["qemu-"+platform.machine()+"-static", "-cpu", MIN_CPUS[platform.machine()],
                              helpers.path_to_executable('test_sig'), sig_name])
 
 if __name__ == "__main__":
